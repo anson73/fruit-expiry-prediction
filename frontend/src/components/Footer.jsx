cf26637bfd983920ca7cf2a78e3b7cbd8860087f
VicTien <victor@neuron.build>
2024-10-02 07:28:38 +1000

    Changed existing stub code to the framework of using react, adapted stub from COMP6080 completed by Victor Tian (z5245580)

---
+++ frontend/src/components/Footer.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+import Typography from "@mui/material/Typography";
+
+const Footer = () => {
+  return (
+    <Typography
+      variant="caption"
+      display="block"
+      gutterBottom
+      sx={{
+        textAlign: "center",
+      }}
+    >
+      Freshness Prediction Engine - COMP3900H11ADigitalHaven 2023 ©
+    </Typography>
+  );
+};
+
+export default Footer;
--- frontend/src/login/login.ts
+++
@@ -1,2 +0,0 @@
-let siteToken = null;
-let userId = null;
