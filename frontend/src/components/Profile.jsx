23f3302795ab4d40706073362d945048c71e002b
VicTien <victor@neuron.build>
2024-10-03 08:09:12 +1000

    Completed profiles page

--- frontend/src/components/Profile.jsx
+++ frontend/src/components/Profile.jsx
@@ -1,36 +1,153 @@
 import React from "react";
+import { useNavigate } from "react-router-dom";
+
 import Typography from "@mui/material/Typography";
 import Avatar from "@mui/material/Avatar";
 import Box from "@mui/material/Box";
 import TextField from "@mui/material/TextField";
+import IconButton from "@mui/material/IconButton";
+import OutlinedInput from "@mui/material/OutlinedInput";
+import InputLabel from "@mui/material/InputLabel";
+import InputAdornment from "@mui/material/InputAdornment";
+import FormControl from "@mui/material/FormControl";
+import Visibility from "@mui/icons-material/Visibility";
+import VisibilityOff from "@mui/icons-material/VisibilityOff";
+import Button from "@mui/material/Button";
 
 const Profile = () => {
+  const [email, setEmail] = React.useState("");
+  const [password, setPassword] = React.useState("");
+  const [newPassword, setNewPassword] = React.useState("");
+  const [newPasswordConfirmed, setNewPasswordConfirmed] = React.useState("");
+  const navigate = useNavigate();
+  const [showPassword, setShowPassword] = React.useState(false);
+  const handleClickShowPassword = () => setShowPassword((show) => !show);
+  const handleMouseDownPassword = (event) => {
+    event.preventDefault();
+  };
+
   return (
     <div
       style={{
+        //border: "1px solid black",
+        height: "100%",
+        //width: "80%",
+        //maxWidth: "20rem",
         display: "flex",
-        flexDirection: "column",
         alignItems: "center",
         justifyContent: "center",
-        gap: "2rem",
+        flexDirection: "column",
+        backgroundColor: "#ffffff",
       }}
     >
-      <Typography
-        variant="caption"
-        display="block"
-        gutterBottom
+      <Box
+        component="form"
         sx={{
-          textAlign: "center",
+          "& > :not(style)": {
+            m: 1,
+            width: "90%",
+            //maxWidth: "20rem",
+          },
+        }}
+        noValidate
+        autoComplete="off"
+        style={{
+          //border: "1px solid red",
+          //height: "80%",
+          width: "80%",
+          maxWidth: "30rem",
+          display: "flex",
+          alignItems: "center",
+          //justifyContent: "center",
+          flexDirection: "column",
+          backgroundColor: "#ffffff",
         }}
       >
-        <h1>THIS IS A PLACEHOLDER FOR THE USER PROFILE PAGE</h1>
-      </Typography>
-      <Avatar
-        alt="Remy Sharp"
-        src="/Users/victor-tien/Downloads/IMG_1727.JPG"
-        sx={{ width: "10rem", height: "10rem", border: "1px solid red" }}
-      />
-      <TextField id="email" required label="Email" variant="outlined" />
+        <Avatar
+          alt="Remy Sharp"
+          src="/static/images/avatar/1.jpg"
+          style={{ width: "15rem", height: "15rem" }}
+        />
+        <TextField
+          id="email"
+          required
+          label="Email"
+          variant="outlined"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+        />
+        <FormControl variant="outlined" required>
+          <InputLabel htmlFor="outlined-adornment-password">
+            Password
+          </InputLabel>
+          <OutlinedInput
+            id="outlined-adornment-password"
+            data-testid="outlined-adornment-password"
+            type={showPassword ? "text" : "password"}
+            endAdornment={
+              <InputAdornment position="end">
+                <IconButton aria-label="toggle password visibility" edge="end">
+                  {showPassword ? <VisibilityOff /> : <Visibility />}
+                </IconButton>
+              </InputAdornment>
+            }
+            label="Password"
+            value={password}
+            required={true}
+            onChange={(e) => setPassword(e.target.value)}
+          />
+        </FormControl>
+        <FormControl variant="outlined" required>
+          <InputLabel htmlFor="outlined-adornment-password">
+            New Password
+          </InputLabel>
+          <OutlinedInput
+            id="outlined-adornment-password"
+            data-testid="outlined-adornment-password"
+            type={showPassword ? "text" : "password"}
+            endAdornment={
+              <InputAdornment position="end">
+                <IconButton aria-label="toggle password visibility" edge="end">
+                  {showPassword ? <VisibilityOff /> : <Visibility />}
+                </IconButton>
+              </InputAdornment>
+            }
+            label="New Password"
+            value={newPassword}
+            required={true}
+            onChange={(e) => setNewPassword(e.target.value)}
+          />
+        </FormControl>
+        <FormControl variant="outlined" required>
+          <InputLabel htmlFor="outlined-adornment-password">
+            New Password Confirmation
+          </InputLabel>
+          <OutlinedInput
+            id="outlined-adornment-password"
+            data-testid="outlined-adornment-password"
+            type={showPassword ? "text" : "password"}
+            endAdornment={
+              <InputAdornment position="end">
+                <IconButton aria-label="toggle password visibility" edge="end">
+                  {showPassword ? <VisibilityOff /> : <Visibility />}
+                </IconButton>
+              </InputAdornment>
+            }
+            label="New Password Confirmation"
+            value={newPasswordConfirmed}
+            required={true}
+            onChange={(e) => setNewPasswordConfirmed(e.target.value)}
+          />
+        </FormControl>
+        <textarea
+          name="postContent"
+          defaultValue="Profile Remarks..."
+          rows="10"
+        />
+        <Button variant="outlined" id="login">
+          Submit
+        </Button>
+      </Box>
     </div>
   );
 };
