e3ca943a02ec31b9dfe9f2034090d5e2915e7994
YicongChen1 <z5292435@ad.unsw.edu.au>
2024-10-05 22:32:32 +1000

    landpage

---
+++ frontend/src/components/Landpage.jsx
@@ -0,0 +1,18 @@
+import React, { useState } from 'react'
+import {
+  Box,
+  Typography,
+} from '@mui/material'
+
+const Landpage = () => {
+  
+  return (
+    <Box sx={{ padding: 50 }}>
+      <Typography variant="h1" align="center">
+        Welcome page
+      </Typography>
+    </Box>
+  )
+}
+
+export default Landpage
