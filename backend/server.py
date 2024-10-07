b82d606f4ec8e6be6295a68a4dd44fbceb227e4a
Diesel Chaisumdet <z5360751@ad.unsw.edu.au>
2024-10-07 13:19:58 +1100

    added sqlite database

--- backend/server.py
+++ backend/server.py
@@ -1,6 +1,46 @@
 from flask import Flask, request
+from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
 
 app = Flask(__name__)
+# Add databse
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///core.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+# Initalise the database
+db = SQLAlchemy(app)
+
+# Create database model
+class users(db.Model):
+    uid = db.Column(db.Integer, primary_key = True)
+    username = db.Column(db.String(50), nullable = False)
+    email = db.Column(db.String(120), nullable = False, unique = True)
+    password = db.Column(db.String(50), nullable = False)
+    reminder_days = db.Column(db.Integer)
+    pfp_id = db.Column(db.Integer)
+    remarks = db.Column(db.String(200))
+
+    def __repr__(self):
+        return '<ID %r>' % self.id
+
+class images(db.Model):
+    pid = db.Column(db.Integer, primary_key = True)
+    prediction = db.Column(db.Integer)
+    feedback = db.Column(db.Integer)
+    upload_date = db.Column(db.DateTime, default=datetime.now(), nullable = False)
+    fruit = db.Column(db.String(20))
+    temperature = db.Column(db.Integer)
+    humidity =  db.Column(db.Integer)
+    def __repr__(self):
+        return '<PID %r>' % self.pid
+
+class user_image(db.Model):
+    uid = db.Column("uid", db.ForeignKey(users.uid))
+    pid = db.Column("pid", db.ForeignKey(images.pid) ,primary_key = True)
+
+    def __repr__(self):
+        return '<PID %r>' % self.pid
+
+
 
 @app.route('/register', methods=['POST'])
 def user_register():
@@ -8,13 +48,16 @@ def user_register():
     Route to create a new user
     return:
     """
-
     user_data = request.json
     user_email = user_data.get("email")
     user_password = user_data.get("password")
     user_name = user_data.get("name")
+    user_id = 0
 
-    return
+    user = users(uid=user_id, username=user_name, email=user_email, password=user_password, reminder_days=None, pfp_id=None, remarks=None)
+    db.session.add(user)
+    db.session.commit()
+    return "Account Created", 201
 
 @app.route('/login', methods=['POST'])
 def user_login():
