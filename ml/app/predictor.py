2a40bab9ca5a5b4fd1e32bdd6e036997cdb2d8a8
Hoang Long <longpaus@gmail.com>
2024-11-03 12:26:04 +1100

    improved the code inside models and predictor

--- ml/app/predictor.py
+++ ml/app/predictor.py
@@ -3,28 +3,34 @@ import torch.nn.functional as F
 from PIL import Image
 from typing import Dict
 
-from app.models import load_model, resnet50_transform
+from app.models import get_decoder, load_model, resnet50_transform
 
 device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 
-apple_model, apple_encoder = load_model("apple", device)
-banana_model,banana_encoder = load_model("banana", device)
-strawberry_model, strawberry_encoder = load_model("strawberry", device)
-mango_model, mango_encoder = load_model("mango", device)
-orange_model, orange_encoder = load_model("orange", device)
+apple_model= load_model("apple", device)
+banana_model= load_model("banana", device)
+strawberry_model= load_model("strawberry", device)
+mango_model= load_model("mango", device)
+orange_model= load_model("orange", device)
+
+apple_decoder = get_decoder('apple')
+banana_decoder = get_decoder('banana')
+mango_decoder = get_decoder('mango')
+orange_decoder = get_decoder('orange')
+strawberry_decoder = get_decoder('strawberry')
 
 # Predict shelf life based on detected fruit class
 def predict_fruit_shelf_life(cropped_image: Image.Image, fruit_class: int) -> Dict[str, str | float]:
     if fruit_class == 0:
-        return predict_shelf_life(cropped_image, apple_model, resnet50_transform, apple_encoder, "apple")
-    elif fruit_class == 1:
-        return predict_shelf_life(cropped_image, banana_model, resnet50_transform, banana_encoder, "banana")
-    elif fruit_class == 2:
-        return predict_shelf_life(cropped_image,mango_model, resnet50_transform, mango_encoder, "mango")
-    elif fruit_class == 3:
-        return predict_shelf_life(cropped_image, orange_model, resnet50_transform, orange_encoder, "orange")
-    elif fruit_class == 4:
-        return predict_shelf_life(cropped_image, strawberry_model, resnet50_transform, strawberry_encoder, "strawberry")
+        return predict_shelf_life(cropped_image, apple_model, resnet50_transform, apple_decoder, "apple")
+    if fruit_class == 1:
+        return predict_shelf_life(cropped_image, banana_model, resnet50_transform, banana_decoder, "banana")
+    if fruit_class == 2:
+        return predict_shelf_life(cropped_image,mango_model, resnet50_transform, mango_decoder, "mango")
+    if fruit_class == 3:
+        return predict_shelf_life(cropped_image, orange_model, resnet50_transform, orange_decoder, "orange")
+    if fruit_class == 4:
+        return predict_shelf_life(cropped_image, strawberry_model, resnet50_transform, strawberry_decoder, "strawberry")
     return {"fruit": "unknown", "prediction": "N/A", "confidence": 0}
 
 # General function to make a prediction using a specified model and transformations
@@ -32,7 +38,7 @@ def predict_shelf_life(
     image: Image.Image,
     model: torch.nn.Module,
     transform,
-    encoder: Dict[int, str],
+    decoder: Dict[int, str],
     fruit_type: str
 ) -> Dict[str, str | float]:
     """
@@ -57,6 +63,6 @@ def predict_shelf_life(
     # Return structured prediction data
     return {
         "fruit": fruit_type,
-        "prediction": encoder[predicted_class],
+        "prediction": decoder[predicted_class],
         "confidence": confidence_score
     }
