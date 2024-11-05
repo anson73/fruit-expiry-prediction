fc10588c80dee2d662f875f843d9be61f6041208
YicongChen1 <z5292435@ad.unsw.edu.au>
2024-11-06 00:46:40 +1100

    update testing

--- frontend/cypress/e2e/Login.cy.jsx
+++ frontend/cypress/e2e/Login.cy.jsx
@@ -9,6 +9,7 @@ describe('<Login />', () => {
     cy.url().should('include', '/login')
   })
   it('renders all fields', () => {
+    // cy.visit('http://localhost:3000/login')
     cy.get('#email').should('be.visible')
     cy.get('#outlined-adornment-password').should('be.visible')
     cy.get('#login').should('be.visible')
@@ -16,6 +17,7 @@ describe('<Login />', () => {
   })
 
   it('wrong email', () => {
+    // cy.visit('http://localhost:3000/login')
     cy.get('#email').type('wrong@email.com')
     cy.get('#outlined-adornment-password').type('wrongPassword')
 
@@ -25,6 +27,7 @@ describe('<Login />', () => {
   })
 
   it('wrong password', () => {
+    // cy.visit('http://localhost:3000/login')
     cy.get('#email').type('z56@email.com')
     cy.get('#outlined-adornment-password').type('wrongPassword')
 
@@ -34,9 +37,21 @@ describe('<Login />', () => {
   })
 
   it('successful logins', () => {
-    
-    cy.get('#email').type('z123')
-    cy.get('#outlined-adornment-password').type('1111')
+    cy.visit('http://localhost:3000/register')
+    cy.get('#email').type('zqq2@gmail.com')
+    cy.get('#userName').type('Oswald')
+    cy.get('#outlined-adornment-password').type('8888')
+    cy.get('#outlined-adornment-password-confirmation').type('8888')
+    cy.contains('The Password does not match! Please double check!').should(
+        'not.exist'
+    )
+    cy.get('#submitButton').click()
+    cy.url().should('include', '/profile')
+    cy.get('#Logout').click()
+
+    cy.visit('http://localhost:3000/login')
+    cy.get('#email').type('zqq2@gmail.com')
+    cy.get('#outlined-adornment-password').type('8888')
 
     cy.intercept('POST', '/login').as('loginRequest')
     cy.get('#login').click()
@@ -46,8 +61,6 @@ describe('<Login />', () => {
       expect(token).to.exist
     })
     cy.url().should('include', '/history')
- 
-
     
   })
 
